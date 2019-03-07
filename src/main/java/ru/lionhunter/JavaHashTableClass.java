e360d5ed45024ff530b376ca8785b46d763eb9e5
Lion-Hunter <01.08.2000>
2019-03-07 17:58:20 +0300

    Added methods contain() for tests and compare(), elements of table currently has sizes of linked lists in the key of Pair.

--- src/main/java/ru/lionhunter/JavaHashTableClass.java
+++ src/main/java/ru/lionhunter/JavaHashTableClass.java
@@ -1,11 +1,11 @@
 package ru.lionhunter;
 
-
 import java.util.Scanner;
 import java.util.LinkedList;
 
 public class JavaHashTableClass {
     public Pair[] array;
+
     public JavaHashTableClass(int quantity) {
         array = new Pair[quantity];
     }
@@ -13,6 +13,13 @@ public class JavaHashTableClass {
     public void create() {
         Scanner scan = new Scanner(System.in);
         JavaHashTableClass arr = new JavaHashTableClass(scan.nextInt());
+    }
+
+    public boolean contain(int elem, Pair[] array) {
+        for (Pair anArray : array) {
+            if (anArray.value.contains(elem)) return true;
+        }
+        return false;
     }
 
     public static int transform(Element object, Pair[] arr) {
@@ -24,14 +31,31 @@ public class JavaHashTableClass {
         Element el = new Element(num);
         int index = transform(el, array);
         LinkedList<Integer> list = array[index].value;
+
         if (!list.contains(num)) {
-            array[index].key = index;
+            array[index].key++;
             list.add(num);
         }
     }
 
     public void remove(int num) {
-        array[num].value.remove(new Integer(num));
+        Element elem = new Element(num);
+        int index = transform(elem, array);
+        array[index].value.remove(new Integer(num));
+        array[index].key--;
+    }
+
+    public boolean compare(Pair[] first, Pair[] second) {
+        if (first.length != second.length) return false;
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i].key != second[i].key) return false;
+
+            for (int sym: first[i].value) {
+                if (!second[i].value.contains(sym)) return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
